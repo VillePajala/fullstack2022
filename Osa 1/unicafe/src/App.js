a872ab765a63e06d51f0a86cc9f9ead3e5c8ea9b
Ville Pajala <valoraami@gmail.com>
2022-07-30 00:03:38 +0300

    1.7 in progress. Added feedback buttons

--- Osa 1/unicafe/src/App.js
+++ Osa 1/unicafe/src/App.js
@@ -6,9 +6,44 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const Button = (props) => {
+    return (
+      <button onClick={props.handleClick}>
+        {props.text}
+      </button>
+    )
+  }
+
+  const Statistics = (props) => {
+    return (
+      <>
+        <p>Good: {props.good} Neutral: {props.neutral} Bad: {props.bad}</p>
+      </>
+    )
+  }
+
+  
+  const positiveFeedback = () => {
+    console.log('Positive feedback')
+    return setGood(good + 1)
+  }
+
+  const neutralFeedback = () => {
+    console.log('Neutral feedback')
+    return setNeutral(neutral + 1)
+  }
+
+  const negativeFeedback = () => {
+    console.log('Negative feedback')
+    return setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <Button handleClick={() => positiveFeedback()} text={'Good'}>Bad</Button>
+      <Button handleClick={() => neutralFeedback()} text={'Neutral'}>Bad</Button>
+      <Button handleClick={() => negativeFeedback()} text={'Bad'}>Bad</Button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
