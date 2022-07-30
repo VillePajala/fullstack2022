f3277ece0c4a13dfeef5f4e4f358c97ffc5a32ca
Ville Pajala <valoraami@gmail.com>
2022-07-30 23:07:16 +0300

    1.6 completed

--- Osa 1/unicafe/src/App.js
+++ Osa 1/unicafe/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
+ 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -36,7 +36,6 @@ const App = () => {
     )
   }
 
-  
   const positiveFeedback = () => {setGood(good + 1)}
   const neutralFeedback = () => {setNeutral(neutral + 1)}
   const negativeFeedback = () => {setBad(bad + 1)}
